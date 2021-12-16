890d0d3afa59c00d4f56d1c6e3db6db00ce534c1
Hrabr <emecandrei83@gmail.com>
2021-12-16 12:52:57 +0200

    add brunch new ff2

--- src/main/java/test/Test1.java
+++ src/main/java/test/Test1.java
@@ -4,6 +4,6 @@ public class Test1 {
     public static void main(String[] args) {
         System.out.println("ddfsdf");
         System.out.println("fsdf");
-
+        System.out.println("dfsdf");
     }
 }
